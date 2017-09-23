04ebd8470109c3d0d70971b7ec26ff4298d616fa
Nikita Kirilov <kirilov.dev@gmail.com>
2017-09-23 21:12:15 +0700

    minor changes

--- bus.hpp
+++ bus.hpp
@@ -1,7 +1,3 @@
-//
-// Created by nikita on 22.09.17.
-//
-
 #ifndef PARADIGMS_BUS_HPP
 #define PARADIGMS_BUS_HPP
 
--- truck.hpp
+++ truck.hpp
@@ -1,7 +1,3 @@
-//
-// Created by nikita on 22.09.17.
-//
-
 #ifndef PARADIGMS_TRUCK_HPP
 #define PARADIGMS_TRUCK_HPP
 
