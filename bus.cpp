b425e6a4647462b23449478bc90317953e58fe7d
Nikita Kirilov <kirilov.dev@gmail.com>
2017-09-27 15:00:21 +0700

    pursue with basic bas and truck structs

--- bus.cpp
+++ bus.cpp
@@ -1,5 +1,14 @@
-//
-// Created by nikita on 22.09.17.
-//
+#include <fstream>
 
 #include "bus.hpp"
+
+
+void input(Bus *bus, std::ifstream &fin)
+{
+	fin >> bus->speed >> bus->passengers_capacity;
+}
+
+void output(Bus *bus, std::ofstream &fout)
+{
+	fout << "Bus: " << bus->speed << " " << bus->passengers_capacity << std::endl;
+}
--- truck.cpp
+++ truck.cpp
@@ -1,5 +1,13 @@
-//
-// Created by nikita on 22.09.17.
-//
+#include <fstream>
 
 #include "truck.hpp"
+
+void input(Truck *truck, std::ifstream &fin)
+{
+	fin >> truck->speed >> truck->goods_capacity;
+}
+
+void output(Truck *truck, std::ofstream &fout)
+{
+	fout << "Truck: " << truck->speed << " " << truck->goods_capacity << std::endl;
+}
