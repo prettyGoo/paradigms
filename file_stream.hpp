21b698ba64022133cd2cbb2e8ab7aeb957d69b0e
Nikita Kirilov <kirilov.dev@gmail.com>
2017-09-27 08:37:32 +0700

    modify file stream sturcture

--- file_stream.hpp
+++ file_stream.hpp
@@ -5,4 +5,18 @@
 #ifndef PARADIGMS_FILE_STREAM_HPP
 #define PARADIGMS_FILE_STREAM_HPP
 
+#include <iostream>
+#include <fstream>
+#include <string>
+
+struct FileStream {
+    std::ifstream ifstream;
+    std::ofstream ofstream;
+
+    std::string ifsteam_name;
+    std::string ofstream_name;
+};
+
+
+
 #endif //PARADIGMS_FILE_STREAM_HPP
