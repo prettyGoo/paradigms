0d0b9c590ffbdaa5a3361678203cc44413fed7ac
Nikita Kirilov <kirilov.dev@gmail.com>
2017-09-27 15:01:43 +0700

    add generic struct

---
+++ transport.cpp
@@ -0,0 +1,56 @@
+#include "transport.hpp"
+#include <fstream>
+#include <iostream>
+
+
+void input(Bus *bus, std::ifstream &fin);
+void input(Truck *truck, std::ifstream &fin);
+
+void output(Bus *bus, std::ofstream &fout);
+void output(Truck *truck, std::ofstream &fout);
+
+
+Transport* input(std::ifstream &fin) {
+	Transport *transport = nullptr;
+
+	int key;
+	fin >> key;
+
+	switch (key)
+	{
+		case Transport::mean::BUS: {
+			transport = new Transport;
+			transport->mean = Transport::mean::BUS;
+			input(&transport->bus, fin);
+			
+			return transport;
+		}
+		case Transport::mean::TRUCK: {
+			transport = new Transport;
+			transport->mean = Transport::mean::TRUCK;
+			input(&transport->truck, fin);
+
+			return transport;
+		}
+		default:
+			return transport;
+	}
+}
+
+
+void output(Transport *transport, std::ofstream &fout)
+{
+	switch (transport->mean)
+	{
+		case Transport::mean::BUS: {
+			output(&transport->bus, fout);
+			break;
+		}
+		case  Transport::mean::TRUCK: {
+			output(&transport->truck, fout);
+			break;
+		}
+		default:
+			break;
+	}
+}
---
+++ transport.hpp
@@ -0,0 +1,23 @@
+#ifndef PARADIGMS_TRANSPORT_HPP
+#define PARADIGMS_TRANSPORT_HPP
+
+#include <fstream>
+
+#include "bus.hpp"
+#include "truck.hpp"
+
+struct Transport {
+	enum mean { BUS = 0, TRUCK = 1};
+	mean mean;
+
+	union
+	{
+		Bus bus;
+		Truck truck;
+	};
+
+	Transport* input(std::ifstream &fin);
+};
+
+
+#endif //PARADIGMS_TRANSPORT_HPP
