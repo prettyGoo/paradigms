2e910433c895cff52afbd39f7566c2c78625c89c
Nikita Kirilov <kirilov.dev@gmail.com>
2017-11-18 12:06:05 +0700

    add to parent abstract class str override to dynamically printing fields of children

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from abc import ABCMeta, abstractmethod
+import inspect
 
 
 class AbstractTransport(metaclass=ABCMeta):
 
-    @abstractmethod
-    def input_data(self):
-        pass
+    # @abstractmethod
+    def __str__(self):
+        string = "{}: ".format(self.__class__.__name__, )
+        for i in inspect.getmembers(self):
+            if not i[0].startswith('_'):
+                if not inspect.ismethod(i[1]):
+                    string += '{} = {} '.format(i[0], i[1])
+
+        return string
 
     @abstractmethod
     def output_date(self):
@@ -19,9 +26,6 @@ class Bus(AbstractTransport):
 
         self.speed = speed
         self.power = power
-
-    def input_data(self):
-        pass
 
     def output_date(self):
         pass
@@ -55,7 +59,7 @@ class Car(AbstractTransport):
         pass
 
 
-
+container = []
 import sys, inspect
 
 with open('in', 'r') as f:
@@ -65,6 +69,18 @@ with open('in', 'r') as f:
             for name, class_obj in inspect.getmembers(sys.modules[__name__]):
                 if inspect.isclass(class_obj) and class_obj != ABCMeta and class_obj != AbstractTransport:
                     if class_obj.id == int(raw_data):
+                        container.append(globals()[name](4, 6))
                         print(name, raw_data)
         else:
             break
+
+
+f.close()
+
+
+with open('out', 'w') as f:
+    for elem in container:
+        print(elem)
+        f.write(str(elem) + '\n')
+
+f.close()
