b804d2df72d27f47aff9c2bb80068ccba1e47310
Nikita Kirilov <kirilov.dev@gmail.com>
2017-11-19 01:43:15 +0700

    pursue client

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from abc import ABCMeta
-
-from transport.bus import Bus
-from transport.truck import Truck
+import sys, inspect
 
 from container.linked_list import LinkedList
+from transport.bus import Bus
+from transport.truck import Truck
+import transport.decorators
+
 
 import transport
 from dataflow.io import IO
@@ -11,81 +13,18 @@ from dataflow.io import IO
 
 package = transport
 
-import pkgutil
+from pkgutil import walk_packages
 submodules = []
-for importer, modname, ispkg in pkgutil.walk_packages(path=package.__path__,
-                                                      prefix=package.__name__+'.',
-                                                      onerror=lambda x: None):
+for _, modname, _ in walk_packages(path=package.__path__, prefix=package.__name__+'.', onerror=lambda x: None):
     submodules.append(modname)
 
 
-
-# class AbstractTransport(metaclass=ABCMeta):
-#
-#     # @abstractmethod
-#     def __str__(self):
-#         string = "{}: ".format(self.__class__.__name__)
-#
-#         extend_string = lambda string, extenders: string + reduce((lambda s, ex: s + ' {} = {}'.format(ex[0], ex[1])), extenders)
-#
-#         print(extend_string(string, [('a', 'a'), ('b', 'bbb')]))
-#         #
-#         # for i in inspect.getmembers(self):
-#         #     if not i[0].startswith('_'):
-#         #         if not inspect.ismethod(i[1]):
-#         #             string += '{} = {} '.format(i[0], i[1])
-#         #
-#         # return string
-#
-#     @abstractmethod
-#     def output_date(self):
-#         pass
-
-
-# class Bus(AbstractTransport):
-#     id = 0
-#
-#     def __init__(self, speed, power):
-#
-#         self.speed = speed
-#         self.power = power
-#
-#     def output_date(self):
-#         pass
-#
-#
-# class Truck(AbstractTransport):
-#     id = 1
-#
-#     def __init__(self, speed, power):
-#         self.speed = speed
-#         self.power = power
-#
-#     def input_data(self):
-#         pass
-#
-#     def output_date(self):
-#         pass
-#
-#
-# class Car(AbstractTransport):
-#     id = 1
-#
-#     def __init__(self, speed, power):
-#         self.speed = speed
-#         self.power = power
-#
-#     def input_data(self):
-#         pass
-#
-#     def output_date(self):
-#         pass
-
-
 container = LinkedList()
-import sys, inspect
+# io = IO(container, 'in', 'out')
 
-b = Bus(1, 1)
+
+# print(sys.modules.keys())
+# sys.exit(1)
 
 with open('in', 'r') as f:
     while True:
@@ -93,17 +32,16 @@ with open('in', 'r') as f:
         if raw_data:
             for submod in submodules:
                 for name, class_obj in inspect.getmembers(sys.modules[submod]):
-                    if inspect.isclass(class_obj) and not inspect.isabstract(class_obj) and class_obj is not ABCMeta:
+                    if inspect.isclass(class_obj) and not inspect.isabstract(class_obj) \
+                            and class_obj is not ABCMeta and not class_obj.__name__.endswith('Decorator'):
                         if class_obj.id == int(raw_data):
                             container.add(globals()[name](4, 6))
-                        # print(name, raw_data)
+                            print(name, raw_data)
         else:
             break
 
 
 f.close()
-
-import sys
 
 with open('out', 'w') as f:
 
