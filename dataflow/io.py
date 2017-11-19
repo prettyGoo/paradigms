df31f900483d8f06bf64eb05b6471fa076d6e0db
Nikita Kirilov <kirilov.dev@gmail.com>
2017-11-19 12:41:52 +0700

    split io code into separate module

--- dataflow/io.py
+++ dataflow/io.py
@@ -1,14 +1,59 @@
 import sys
+import transport
+import inspect
+from abc import ABCMeta
+package = transport
+
+
+from pkgutil import walk_packages
+submodules = []
+for _, modname, _ in walk_packages(path=package.__path__, prefix=package.__name__+'.', onerror=lambda x: None):
+    submodules.append(modname)
 
 
 class IO(object):
     def __init__(self, source, stdin_path, stdout_path):
-        self.source = source
         self.stdin_path = stdin_path
         self.stdout_path = stdout_path
+        self.source = source
 
-    def reset_stdin_path(self, new_path):
-        self.stdin_path = new_path
+    def read(self):
+        with open(self.stdin_path, 'r') as f:
+            self._prepare_entities_objects_list()
+            raw_data = f.readline()
 
-    def reset_stdout_path(self, new_path):
-        self.stdout_path = new_path
+            while raw_data:
+                data = raw_data.split(' ')
+                self._create_entity_from_data(data)
+
+                raw_data = f.readline()
+
+    def write(self):
+        with open(self.stdout_path, 'w') as f:
+            active_node = self.source.head
+
+            if active_node is None:
+                return
+
+            while active_node.next is not None:
+                data = active_node.get_data()
+                print(data, file=f)
+
+                active_node = active_node.get_next()
+
+    def _prepare_entities_objects_list(self):
+        class_objects = []
+
+        for submod in submodules:
+            for name, class_obj in inspect.getmembers(sys.modules[submod]):
+                if inspect.isclass(class_obj) and not inspect.isabstract(class_obj) \
+                        and class_obj is not ABCMeta and not class_obj.__name__.endswith('Decorator'):
+                    class_objects.append(class_obj)
+
+        self.class_objects = class_objects
+
+    def _create_entity_from_data(self, data):
+        for entity in self.class_objects:
+            if entity.id == int(data[0]):
+                self.source.add(entity(4, 6))
+                print(entity.__name__, data[0])
--- main.py
+++ main.py
@@ -1,57 +1,18 @@
-from abc import ABCMeta
-import sys, inspect
-
 from container.linked_list import LinkedList
+
 from transport.bus import Bus
 from transport.truck import Truck
-import transport.decorators
+from transport.decorators import SpeedDecorator, WeightToPowerBusDecorator, WeightToPowerTruckDecorator
 
-
-import transport
 from dataflow.io import IO
 
+if __name__ == '__main__':
 
-package = transport
+    container = LinkedList()
+    io = IO(container, 'in', 'out')
+    io.read()
+    io.write()
 
-from pkgutil import walk_packages
-submodules = []
-for _, modname, _ in walk_packages(path=package.__path__, prefix=package.__name__+'.', onerror=lambda x: None):
-    submodules.append(modname)
+    container.clean()
 
-
-container = LinkedList()
-# io = IO(container, 'in', 'out')
-
-
-# print(sys.modules.keys())
-# sys.exit(1)
-
-with open('in', 'r') as f:
-    while True:
-        raw_data = f.readline().replace('\n', '')
-        if raw_data:
-            for submod in submodules:
-                for name, class_obj in inspect.getmembers(sys.modules[submod]):
-                    if inspect.isclass(class_obj) and not inspect.isabstract(class_obj) \
-                            and class_obj is not ABCMeta and not class_obj.__name__.endswith('Decorator'):
-                        if class_obj.id == int(raw_data):
-                            container.add(globals()[name](4, 6))
-                            print(name, raw_data)
-        else:
-            break
-
-
-f.close()
-
-with open('out', 'w') as f:
-
-    active_node = container.head
-    sys.exit(0) if active_node is None else None
-
-    while active_node.next is not None:
-        data = active_node.get_data()
-        print(data, file=f)
-
-        active_node = active_node.get_next()
-
-f.close()
+    print('Finished')
