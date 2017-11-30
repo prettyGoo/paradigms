848cf1e320193adf4d4f12a66ae7951c8da3ecb7
Nikita Kirilov <kirilov.dev@gmail.com>
2017-11-30 17:23:14 +0700

    init tests

--- tests/tests.py
+++ tests/tests.py
@@ -1,7 +1,35 @@
-# I/O
-# 1. Test that deal with absent input.txt
-# 2. Test that deal with absent out.txt
-# 3. Test that input file format is correct
-
-
 import unittest
+
+from transport.bus import Bus
+from transport.truck import Truck
+
+from container.linked_list import LinkedList
+
+
+class TestContainer(unittest.TestCase):
+
+    b = Bus([1, 1])
+    b1 = Bus([1, 1])
+
+    t = Truck([2, 2])
+    t1 = Truck([2, 2])
+
+    l = LinkedList()
+    l.add(b)
+    l.add(t)
+    l.add(b1)
+    l.add(t1)
+
+    def test_add(self):
+        length = 0
+        active_node = self.l.head
+
+        while active_node is not None:
+            length += 1
+            active_node = active_node.next
+
+        self.assertEqual(length, 4)
+
+    def test_clean(self):
+        self.l.clean()
+        self.assertEqual(self.l.head, None)
